fd36d890b0be196a8eefb642e32312c4b2e506bb
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2023-08-10 20:01:07 -0400

    client: add colors to tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,13 @@ export default {
 		extend: {
 			fontFamily: {
 				notosans: ['Noto Sans', 'sans-serif']
+			},
+			colors: {
+				'columbia-blue': '#d1dbe4',
+				'cadet-blue': '#a3b7ca',
+				'weldon-blue': '#7593af',
+				'queen-blue': '#476f95',
+				'dark-cerulean': '#194a7a'
 			}
 		}
 	},
