65807a769850807d9fc927c5c588fc7e93d08b4e
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2023-08-13 16:31:50 -0400

    client: validation functions

---
+++ client/src/utils/fns.ts
@@ -0,0 +1,11 @@
+export const validateEmail = (email: string) => {
+	return String(email)
+		.toLowerCase()
+		.match(
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		);
+};
+
+export const validatePhoneNumber = (phone_number: string) => {
+	return String(phone_number).match(/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/);
+};
