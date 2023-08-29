6024724f5fbfc534905763a0dffde0176a8a339f
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2023-08-29 12:03:52 -0400

    client: move layout to layout page

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
-  import '../app.css';
+	import '../app.css';
 </script>
 
-<slot />
+<div class="min-h-screen md:h-screen md:min-h-0">
+	<slot />
+	<footer class="flex h-20 items-center justify-center text-sm font-light">
+		<div class="text-center">
+			<p>&#169; {new Date().getFullYear()} Ahmed Alhassan. All Rights Reserved.</p>
+			<div class="mt-2">
+				<a href="/privacy" class="text-weldon-blue mr-2">Privacy Policy</a>
+				<a href="/terms-conditions" class="text-weldon-blue">Terms and Conditions</a>
+			</div>
+		</div>
+	</footer>
+</div>
