cb21fd3405e805b70450f306b35637703debd31e
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2023-09-04 21:00:16 -0400

    client: success page

--- client/src/routes/success/+page.svelte
+++ client/src/routes/success/+page.svelte
@@ -1,1 +1,11 @@
-<h1>This is a success</h1>
+<div class="flex items-center justify-center sm:h-[calc(100vh-80px)]">
+	<div>
+		<div class="mx-auto w-32">
+			<img src="/undraw_greek_freak_j-2-qb.svg" alt="The Greek Freak illustration" />
+		</div>
+		<p class="mt-8 text-center text-2xl">Awesome!!</p>
+		<p class="mt-4 text-center">
+			You'll receive an email and/or text message with details about signing up and drafting.
+		</p>
+	</div>
+</div>
