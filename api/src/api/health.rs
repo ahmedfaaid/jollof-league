cf3d020d766b2f03b1c26184636c499b689edfa8
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2023-10-04 20:57:08 -0400

    api: return json from health check

--- api/src/api/health.rs
+++ api/src/api/health.rs
@@ -1,4 +1,12 @@
+use rocket::serde::json::Json;
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct HealthCheckResponse {
+    status: &'static str,
+}
+
 #[get("/health")]
-pub fn health_check() -> &'static str {
-    "Health check successful!"
+pub fn health_check() -> Json<HealthCheckResponse> {
+    Json(HealthCheckResponse { status: "OK" })
 }
