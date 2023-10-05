fce6f9bbd425eda8483988396b070ddb63735d12
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2023-10-04 21:32:51 -0400

    schedule: to keep server awake daily

---
+++ schedule/app.js
@@ -0,0 +1,15 @@
+import { CronJob } from 'cron';
+import 'dotenv/config';
+
+const mainExpression = '*/14 * * * *';
+const twoSecs = '*/10 * * * * *';
+
+const job = new CronJob(twoSecs, async function () {
+  const req = await fetch(process.env.JOLLOF_HEALTH_CHECK);
+  const res = await req.json();
+
+  console.log(res);
+  console.log('Ya!');
+});
+
+job.start();
